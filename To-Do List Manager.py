7fee789d449ec34c629464a7e0794124bfabd824
Ahmed <ahmedelshekih@gmail.com>
2024-12-01 12:27:51 +0200

    add a loop

--- To-Do List Manager.py
+++ To-Do List Manager.py
@@ -1,11 +1,20 @@
 # Allow users to add, delete, and view tasks.
 # Save tasks to a file for persistence.
 def menu():
+    global isrunning
     print("Main menu")
-    print("[1] Add Task \n [2] View Tasks ")
+    print("[1] Add Task \n[2] View Tasks ")
     print("[q] Quit")
 
-    choice = input("choose an option").lower()
+    choice = input("choose an option : ").lower()
+
+    # if choice == '1':
+    #     addTask()
+    # elif choice == '2':
+    #     TaskView()
+    # elif choice.lower() == 'q':
+    #     isrunning = False
+
 
     match choice:
         case '1':
@@ -14,8 +23,9 @@ def menu():
             TaskView()
         case 'q':
             isrunning = False
-            return isrunning
+    
 
+    return isrunning
 
 def addTask():
     while True:
@@ -23,20 +33,24 @@ def addTask():
         toDoNum = input("Enter your task number : ")
         toDoList.update({ toDoNum : toDo })
         print(toDoList)
+
         if input("do you want to add any other tasks? [y/n] : ").lower() == "n":
             break
 
 def TaskView():
-    for taskNum,task in toDoList:
+    for taskNum,task in toDoList.items():
         print(f"{taskNum} : {task}")
+    
+
+
+
 
 
 toDoList = {}
 isrunning = True
-addTask()
 
-if input("do you want to add any other tasks? [y/n] : ").lower() == "y":
-    addTask()
+while isrunning:
+    menu()
 
 
 
